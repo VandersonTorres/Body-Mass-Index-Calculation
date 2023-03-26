38dd8b3f38a1f0c939a8494b3a2b791e2f9e0dc5
Vanderson <vanderson.torres1@hotmail.com>
2023-03-25 22:32:55 -0300

    fix bug issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,16 @@
 function myEscope() {                                   // Function of not interviewing at Global Escope
 
     /* IMC = Índice de Massa Corporal (Body Mass Index) */
-
-    const title = document.querySelector(".title");
-    title.innerHTML = "Tabela IMC";
-
+    const form = document.querySelector("#form");
+    const inputWeight = document.querySelector(".weight");
+    const inputHeight = document.querySelector(".height");
+    const result = document.querySelector(".result");
     const info = document.querySelector("#ideal-weight");
-    info.innerHTML = "";
-    info.classList.add("hide");
-
-    const form = document.querySelector("#form")
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        const inputWeight = document.querySelector(".weight");
-        const inputHeight = document.querySelector(".height");
-        const weight = Number(inputWeight.value);
-        let height = Number(inputHeight.value);
-        if (height >= 3) {
-            height /= 100;
-        };
-
-        if (!weight || weight <= 0) {                   // Weight Value Unexpected
-            let msg = "Peso Inválido!";
-            setResult(msg, false);
-            return;
-        }
-        if (!height || height <= 0) {                   // Height Value Unexpected
-            let msg = "Altura Inválida!";
-            setResult(msg, false);
-            return;
-        }
-
-        const imc = calculateIMC(weight, height);
-        const situation = situationIMC(imc);
-        const maxIMC = (imc <= 24.9);
-        const minIMC = (imc >= 18.5);
-        let msg = `Seu IMC é ${imc}! ${situation}`;
-        setResult(msg, true);
-        
-        if (maxIMC === false) {
-            return maxWeight(weight, height);
-        }
-        if (minIMC === false) {
-            return minWeight(weight, height);
-        }
-    });
+    const liquid = document.querySelector("#liquid");
+    const kcal = document.querySelector("#kcal");
+    const slimmingKCAL = 30;
+    const maintenanceKCAL = 35;
+    const mlWater = 35;
 
     function calculateIMC(weight, height) {             // Function to calculate Body Mass Index (portuguese - IMC)
         const calculation = (weight / (height ** 2));
@@ -54,9 +19,9 @@ function myEscope() {                                   // Function of not inter
 
     function situationIMC(imc) {                        // To verify weight situation
         const situation = [
-            "Você está abaixo do peso!", "Você está com um peso normal!",
-            "Você está com sobrepeso!", "Você está com obesidade grau 1!",
-            "Você está com obesidade grau 2!", "Você está com obesidade grau 3!",
+            "Abaixo do peso!", "Peso normal!",
+            "Sobrepeso!", "Obesidade grau 1!",
+            "Obesidade grau 2!", "Obesidade grau 3!",
         ];
 
         if (imc < 18.5) return situation[0];
@@ -67,32 +32,60 @@ function myEscope() {                                   // Function of not inter
         if (imc > 39.9) return situation[5];
     };
 
-    function maxWeight (weight, height) {
+    function maxWeight(weight, height) {
         info.classList.remove("hide");
         const calcMax = (24.9 * (height ** 2));
         const action = (weight - calcMax);
         const paragraph = document.createElement("p");
-        paragraph.innerHTML = `
-        O seu 'peso máximo' ideal, seria de: <mark> ${calcMax.toFixed(2)}Kg </mark> <br />
-        Você precisa emagrecer <mark> ${action.toFixed(2)}Kg </mark>
-        `;
+        paragraph.innerHTML = `Peso Máximo Recomendado: <mark> ${calcMax.toFixed(2)}Kg </mark> <br />`;
+        if (action > 0) {
+            paragraph.innerHTML += `Situação: Emagrecer <mark> ${action.toFixed(2)}Kg </mark>`;        }
         info.appendChild(paragraph);
     };
 
-    function minWeight (weight, height) {
+    function minWeight(weight, height) {
         info.classList.remove("hide");
         const calcMin = (18.5 * (height ** 2));
         const action = (calcMin - weight);
         const paragraph = document.createElement("p");
         paragraph.innerHTML = `
-        O seu 'peso mínimo' ideal, seria de: <mark> ${calcMin.toFixed(2)}Kg </mark> <br />
-        Você precisa ganhar <mark> ${action.toFixed(2)}Kg </mark>
+        Peso Mínimo Recomendado: <mark> ${calcMin.toFixed(2)}Kg </mark> <br />
+        Situação: Ganhar <mark> ${action.toFixed(2)}Kg </mark>
         `;
         info.appendChild(paragraph);
     };
 
+    function verifyWater(weight) {
+        liquid.classList.remove("hide");
+        liquid.innerHTML = "";
+        const amountWater = ((weight * mlWater) / 1000);
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = `
+        Ingestão de líquido: <mark> ${amountWater.toFixed(2)}L </mark>/ Dia`;
+        liquid.appendChild(paragraph);
+    };
+
+    function kcalLoseWeight(weight) {
+        kcal.classList.remove("hide");
+        kcal.innerHTML = "";
+        let amountKCAL = (weight * slimmingKCAL);
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = `
+        Necessidade calórica: <mark> ${Math.round(amountKCAL)}Kcal </mark>/ Dia`;
+        kcal.appendChild(paragraph);
+    };
+
+    function kcalMaintenance(weight) {
+        kcal.classList.remove("hide");
+        kcal.innerHTML = "";
+        let amountKCAL = (weight * maintenanceKCAL);
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = `
+        Necessidade calórica: <mark> ${Math.round(amountKCAL)}Kcal </mark>/ Dia`;
+        kcal.appendChild(paragraph);
+    };
+
     function setResult(msg, isValid) {
-        const result = document.querySelector(".result");
         result.innerHTML = "";
         info.innerHTML = "";
         info.classList.add("hide");
@@ -103,6 +96,48 @@ function myEscope() {                                   // Function of not inter
         result.appendChild(paragraph);
     };
 
-}
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const weight = Number(inputWeight.value);
+        let height = Number(inputHeight.value);
+
+        if (height >= 3) {
+            height /= 100;
+        };
+
+        if (weight <= 0) {                   // Weight Value Unexpected
+            let msg = "Peso Inválido!";
+            setResult(msg, false);
+            return;
+        };
+
+        if (height <= 0) {                   // Height Value Unexpected
+            let msg = "Altura Inválida!";
+            setResult(msg, false);
+            return;
+        };
+
+        const imc = calculateIMC(weight, height);
+        const situation = situationIMC(imc);
+        const maxIMC = (imc <= 24.9);
+        const minIMC = (imc >= 18.5);
+        let msg = `IMC: ${imc}<br /> Diagnóstico: ${situation}`;
+        setResult(msg, true);
+        verifyWater(weight);
+
+        if (maxIMC === false && minIMC === true) {
+            return maxWeight(weight, height),
+            kcalLoseWeight(weight);
+        } else if (maxIMC === true && minIMC === true) {
+            return kcalMaintenance(weight), 
+            maxWeight(weight, height);
+        } else if (maxIMC === true && minIMC === false) {
+            return minWeight(weight, height),
+            kcal.classList.add("hide");
+        };
+
+    });
+
+};
 
 myEscope();
